25e8417f8fce4f9b1e8df7b4f840c8e2e529477f
Priyansh kushwaha <priyanshkushwaha10@gmail.com>
2022-04-30 18:56:56 +0530

    array

---
+++ Array/Array.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 2, 7 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
